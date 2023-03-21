8c8fe4f709440e1652dcbb4094145ae63d5e9507
javierOrtega95 <fjortega@wizzie.io>
2023-03-22 00:00:16 +0100

    feat: check winner and end game

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Square } from './components/Square';
+import { WinnerModal } from './components/WinnerModal';
 
 const TURNS = {
   X: 'x',
   O: 'o',
 };
 
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`;
-
-  const handleClick = () => {
-    updateBoard(index);
-  };
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  );
+const WINNER = {
+  X: 'x',
+  O: 'o',
+  Draw: 'draw',
+  NoWinner: null,
 };
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(TURNS.X);
 
+  const [winner, setWinner] = useState(WINNER.NoWinner);
+
+  const checkWinner = boardToCheck => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+
+    return WINNER.NoWinner;
+  };
+
+  const checkEndGame = boardToCheck =>
+    boardToCheck.every(square => square !== null);
+
   const updateBoard = index => {
-    if (board[index]) return;
+    if (board[index] || winner) return;
 
     const newBoard = [...board];
     newBoard[index] = turn;
@@ -36,6 +60,20 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+
+    if (newWinner) {
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(WINNER.Draw);
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(WINNER.NoWinner);
   };
 
   return (
@@ -54,6 +92,8 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
   );
 }
---
+++ src/components/WinnerModal.jsx
@@ -0,0 +1,21 @@
+import { Square } from './Square.jsx';
+
+export const WinnerModal = ({ winner, resetGame }) => {
+  if (winner === null) return null;
+
+  const winnerText = winner === false ? 'Draw' : 'Win:';
+
+  return (
+    <section className='winner'>
+      <div className='text'>
+        <h2>{winnerText}</h2>
+
+        <header className='win'>{winner && <Square>{winner}</Square>}</header>
+
+        <footer>
+          <button onClick={resetGame}>Reset game</button>
+        </footer>
+      </div>
+    </section>
+  );
+};
